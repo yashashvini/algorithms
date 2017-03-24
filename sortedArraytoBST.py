80714fccc38e0c9a57485f4bd18adf684dae25a0
yash_pakala <yashashvinipakala@gmail.com>
2017-03-24 12:47:24 -0400

    sorted array to bst

---
+++ sortedArraytoBST.py
@@ -0,0 +1,24 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def sortedArrayToBST(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: TreeNode
+        """
+        if(len(nums)>1):
+            mid = (len(nums))/2
+            root = TreeNode(nums[mid])
+            root.left = self.sortedArrayToBST(nums[:mid])
+            root.right = self.sortedArrayToBST(nums[mid+1:])
+            return root
+        elif(len(nums)==1):
+            temp = TreeNode(nums[0])
+            return temp
+        else:
+            return None
